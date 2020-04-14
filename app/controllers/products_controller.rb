a60c05d39cbc0715b8f1211866563a95e62d5a95
高雲大生 <takakumohiorki@takakumodaiseinoMacBook-Air.local>
2020-04-14 16:10:23 +0900

    add products/check scss

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,5 +11,24 @@ class ProductsController < ApplicationController
   def check
     @product = Product.find_by(id: params[:id])
     @order = Order.new
+    if @price = Price.find_by(id: 1)
+      @price = Price.find_by(id: 1)
+    else
+      @price = Price.new
+      @price.count = 1
+      @price.price = 0
+    end
+
+    @order.count = 1
+    @order.user_id = 1
+    @order.product_id = @product.id
+
+    @price.price += @product.money
+
+    @order.save
+    @price.save
+
+    @orders = Order.where(count: 1)
   end
+  
 end
