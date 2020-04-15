b291db9cded0928a0e638ae90a16a72f95c41591
高雲大生 <takakumohiorki@takakumodaiseinoMacBook-Air.local>
2020-04-15 15:30:35 +0900

    add users/edit html.scss

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,25 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(name:params[:name],
-                    email:params[:email],
-                    password:params[:password],
-                    delete_id:0,
-                    admin_id:0,
-                    count:1)
+    @user = User.new(name: params[:name],
+                    email: params[:email],
+                    password: params[:password],
+                    delete_id: 0,
+                    admin_id: 0,
+                    count: 1)
+    @user.save
+    render("users/index")
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    @user.password = params[:password]
     @user.save
     render("users/index")
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'users/index' => 'users#index'
   get 'users/new' => 'users#new'
   post 'users/create' => 'users#create'
+  get 'users/edit/:id' => 'users#edit'
+  post 'users/update/:id' => 'users#update'
 
   get 'products/index' => 'products#index'
   get 'products/show/:id' => 'products#show'
