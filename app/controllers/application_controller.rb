b8d92facb882f2c1c260c5a4fab16a6b7d011cd9
高雲大生 <takakumohiorki@takakumodaiseinoMacBook-Air.local>
2020-04-16 23:09:47 +0900

    add master/index html,scss

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
     before_action :set_current_user
+    before_action :header_color
 
 
     def set_current_user
         @current_user = User.find_by(id: session[:user_id])
+    end
+
+    def header_color
+        @header_color = "user_user"
+        @header_menu_color = "header-menus"
     end
 end
--- app/controllers/masters_controller.rb
+++ app/controllers/masters_controller.rb
@@ -1,4 +1,10 @@
 class MastersController < ApplicationController
+  before_action :header_color_admin
+
+  def header_color_admin
+    @header_color = "admin"
+    @header_menu_color = "header-menus_admin"
+  end
 
   def index
     @user = User.find_by(id: session[:user_id])
